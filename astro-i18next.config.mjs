797bc3881f7d4d1f68566dd2e41bed7f99e53bfe
felixadlen <felixadlen@gmail.com>
2024-04-03 18:40:14 +0800

    eventual fix for internalization netlify

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,10 +1,26 @@
+import en from ‘./public/locales/en/translation.json’ assert { type: ‘json’ }
+import sv from ‘./public/locales/sv/translation.json’ assert { type: ‘json’ }
+
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
   defaultLocale: "en",
   locales: ["en", "sv"],
   i18nextServer: {
+    resources: {
+      en: {
+      translation: {
+      en
+      }
+      },
+      sv: {
+      translation: {
+    sv
+      }
+      }
+      }
     backend: {
       loadPath: './public/locales/{{lng}}/{{ns}}.json',
+   
     },
   },
 };
