3bd2eeccec7bdd482300fe0f044318216dc25eb8
felixadlen <felixadlen@gmail.com>
2024-04-05 01:42:58 +0800

    added sitemap filter to noindex two pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,15 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   site: settings.site,
+<<<<<<< HEAD
   integrations: [sitemap(), mdx(),astroI18next()],
+=======
+  integrations: [sitemap({
+    filter: (page) =>
+    page !== 'https://xzstudios.se/thankyou/' &&
+    page !== 'https://xzstudios.se/secrets/' ,
+  }), mdx()],
+>>>>>>> 7a3f3f6 (added sitemap filter to noindex two pages)
   vite: {
     ssr: {
       noExternal: ['astro', '@igor.dvlpr/astro-post-excerpt']
